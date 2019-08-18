37550feefa030c22db016d326116aef5e0e53642
SemihBogaz <sbogazq@gmail.com>
2019-08-18 12:16:11 +0300

    Flight & cities classes

---
+++ Flight.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from Airport.cities import Cities
+
+
+class Flight:
+    def __init__(self, departure: Cities, destination: Cities, dates: datetime):
+        self.__Destination = destination
+        self.__Departure = departure
+        self.__Dates = dates
+
+    def tardiness(self, minutes: int):
+        Day = self.__Dates.day
+        Hour = self.__Dates.hour
+        Minute = self.__Dates.minute
+
+        if (Minute + minutes) >= 60:
+            Hour += int((Minute + minutes) / 60)
+            Minute = (Minute + minutes) % 60
+
+            if Hour >= 24:
+                Day += int(Hour / 24)
+                Hour = Hour % 24
+
+        newDate = datetime(self.__Dates.year, self.__Dates.month, Day, Hour, Minute)
+        self.__Dates = newDate
+
+    def get_date(self):
+        return self.__Dates
+
+
+x = Flight(Cities("İstanbul"), Cities("İzmir"), datetime(2019, 8, 2, 7, 40))
+print(str(x.get_date().hour) + "." + str(x.get_date().minute))
+x.tardiness(int(30))
+print(str(x.get_date().hour) + "." + str(x.get_date().minute))
+
+
+
+
---
+++ cities.py
@@ -0,0 +1,107 @@
+from datetime import datetime
+from random import randint
+
+c = """Adana
+Adıyaman
+Afyon 
+Ağrı 
+Amasya
+Ankara
+Antalya
+Artvin
+Aydın
+Balıkesir
+Bilecik
+Bingöl
+Bitlis
+Bolu
+Burdur
+Bursa
+Çanakkale
+Çankırı
+Çorum
+Denizli
+Diyarbakır
+Edirne
+Elazığ
+Erzincan
+Erzurum
+Eskişehir
+Gaziantep
+Giresun
+Gümüşhane
+Hakkari
+Hatay
+Isparta
+İçel
+İstanbul
+İzmir
+Kars
+Kastamonu
+Kayseri
+Kırklareli
+Kırşehir
+Kocaeli
+Konya
+Kütahya
+Malatya
+Manisa
+K.maraş
+Mardin
+Muğla
+Muş
+Nevşehir
+Niğde
+Ordu
+Rize
+Sakarya
+Samsun
+Siirt
+Sinop
+Sivas
+Tekirdağ
+Tokat
+Trabzon
+Tunceli
+Şanlıurfa
+Uşak
+Van
+Yozgat
+Zonguldak
+Aksaray
+Bayburt
+Karaman
+Kırıkkale
+Batman
+Şırnak
+Bartın
+Ardahan
+Iğdır
+Yalova
+Karabük
+Kilis
+Osmaniye
+Düzce"""
+
+cities = c.splitlines()
+
+
+class Cities:
+    def __init__(self, name: str):
+        self.__Name = name
+        self.__Temperature = randint(18, 25)
+        self.__Weather = ["Sunny", "Rainy", "Foggy", "Windy"][randint(0, 3)]
+
+    def get_name(self):
+        return self.__Name
+
+    def set_name(self, name):
+        self.__Name = name
+
+    def get_temp(self):
+        return self.__Temperature
+
+    def get_weather(self):
+        return self.__Weather
+
+    # did not create setter methods for temp & weather
